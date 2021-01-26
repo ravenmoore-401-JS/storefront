3d0208811bec907091d148e91fd1c87b3bad4a7d
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-01-25 23:36:05 -0800

    error fixes for react sillyness

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Categories from './components/categories/category';
